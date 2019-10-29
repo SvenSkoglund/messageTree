e0427ca451040e0e25ef0699506407b000d9ab4e
Xian Zheng <xzheng2112@gmail.com>
2019-10-29 09:04:24 -0600

    endpoint call in MessageTree

--- message-tree-ui/src/components/MessageTree.vue
+++ message-tree-ui/src/components/MessageTree.vue
@@ -1,17 +1,41 @@
 <template>
-    <div>
+    <div id="messageBoard">
         <h2>Message Tree Board</h2>
         <div>
-
+            {{messages}}
         </div>
     </div>
 </template>
 
 <script>
+    import api from './backend-api';
     export default {
+        el: "messageBoard",
         name: "MessageTree",
-        props: ["messages"]
+        data() {
+            return this.messages = this.allMessages();
+        },
+        created(){
+            this.allMessages();
+        },
+        methods : {
+            allMessages()
+            {
+                api.getAll()
+                    .then(response => {
+                        this.$log.debug("Data loaded: ", response.data)
+                        this.messages = response.data
+                    })
+                    .catch(err => {
+                        this.$log.debug(err)
+                        this.err = "Failed to load messages"
+                    })
+                    .finally(() => this.loading = false);
+            },
+
+        }
     }
+
 </script>
 
 <style scoped>
