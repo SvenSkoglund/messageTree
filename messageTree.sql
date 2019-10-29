7329df93662cfd1607efc642d907fefcdde2feaa
Sven Skoglund <svenrskoglund@gmail.com>
2019-10-29 10:34:28 -0600

    added junk data to sql

--- messageTree.sql
+++ messageTree.sql
@@ -46,3 +46,19 @@ GRANT ALL ON * TO 'user';
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `messageTreeDB`.`message`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `messageTreeDB`;
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (1, 'This is a message', NULL, 2, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (2, 'This is a comment', 1, 3, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (3, 'This is a comment on a comment', 2, -3, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (4, 'This is another comment on a comment', 2, -2, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (5, 'Yet another comment on a comment', 2, -1, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (6, 'This is a new message', , 3, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (7, 'This is a commet on the new message', 6, 10, DEFAULT);
+INSERT INTO `messageTreeDB`.`message` (`id`, `content`, `parentID`, `likes`, `timestamp`) VALUES (8, 'Another comment on that new message', 6, -9, DEFAULT);
+
+COMMIT;
